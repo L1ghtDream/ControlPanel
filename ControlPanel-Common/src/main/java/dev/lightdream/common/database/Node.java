4669b29f4937d787f72fab4978cdc50cab4a884e
LightDream <radu.voinea.mihai@gmail.com>
2022-04-21 00:34:42 +0300

    Local terminal - Needs testing

--- ControlPanel-Common/src/main/java/dev/lightdream/common/database/Node.java
+++ ControlPanel-Common/src/main/java/dev/lightdream/common/database/Node.java
@@ -6,11 +6,17 @@ import dev.lightdream.common.dto.permission.PermissionEnum;
 import dev.lightdream.common.manager.SSHManager;
 import dev.lightdream.databasemanager.annotations.database.DatabaseField;
 import dev.lightdream.databasemanager.annotations.database.DatabaseTable;
+import dev.lightdream.logger.Debugger;
 import lombok.NoArgsConstructor;
 import lombok.SneakyThrows;
 
-import java.io.ByteArrayOutputStream;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
 @DatabaseTable(table = "nodes")
@@ -49,6 +55,21 @@ public class Node extends PermissionContainer {
 
     @SneakyThrows
     public String executeCommand(String command) {
+        Debugger.log(command);
+        AtomicReference<String> output = new AtomicReference<>();
+
+        Process process = Runtime.getRuntime().exec(command);
+        StreamGobbler streamGobbler = new StreamGobbler(process.getInputStream(), output::set);
+        Executors.newSingleThreadExecutor().submit(streamGobbler);
+        int exitCode = process.waitFor();
+        assert exitCode == 0;
+
+        return output.get();
+    }
+
+    /*
+    @SneakyThrows
+    public String executeCommand(String command) {
         SSHManager.NodeSSH ssh = getSSH();
         SSHManager.SSHSession session = ssh.createNew();
         session.setCommand(command);
@@ -63,10 +84,28 @@ public class Node extends PermissionContainer {
 
         return responseStream.toString();
     }
+    */
+
+    private static class StreamGobbler implements Runnable {
+        private final InputStream inputStream;
+        private final Consumer<String> consumer;
+
+        public StreamGobbler(InputStream inputStream, Consumer<String> consumer) {
+            this.inputStream = inputStream;
+            this.consumer = consumer;
+        }
+
+        @Override
+        public void run() {
+            new BufferedReader(new InputStreamReader(inputStream)).lines()
+                    .forEach(consumer);
+        }
+    }
 
     @SuppressWarnings("UnusedReturnValue")
     public String sendCommandToServer(String command, Server server) {
-        return executeCommand("screen -S " + server.serverID + " -X stuff '" + command + "\\n'");
+        return executeCommand("screen -S " + server.serverID + " -X stuff '" + command + "^M'");
+        //return executeCommand("screen -S " + server.serverID + " -X stuff '" + command + "\\n'");
     }
 
     public List<Server> getServers() {
