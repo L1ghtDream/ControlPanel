8133f1c1524853a6416ac170a5da3ea77413f45e
LightDream <radu.voinea.mihai@gmail.com>
2022-04-27 22:41:33 +0300

    updated web_install.sh to install nginx. [Needs to add configuration]

--- scripts/web/web_install.sh
+++ scripts/web/web_install.sh
@@ -4,6 +4,7 @@ version="1.0.0";
 # Installing dependencies
 sudo apt-get install screen                                                     # Web server
 sudo apt-get install openjdk-8-jdk openjdk-11-jdk openjdk-16-jdk openjdk-17-jdk # Java
+sudo apt-get install nginx                                                      # Web Proxy
 
 # Get the start script and make it executable
 wget https://raw.githubusercontent.com/L1ghtDream/ControlPanel/master/scripts/web/web_start.sh start.sh
@@ -11,5 +12,9 @@ chmod u+x start.sh
 
 # Get the jar from GitHub releases
 wget "https://github.com/L1ghtDream/ControlPanel/releases/download/latest/ControlPanel-Web-${version}.jar" -O ControlPanel-Web.jar
+
+# Setup web-server
+mkdir "/var/www/html/google_auth"
+mkdir "/var/www/html/google_auth/qr"
 
 # Add to crontab
