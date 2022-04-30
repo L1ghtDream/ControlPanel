8ba32faec834a21ab73e59446bd7f4a365f78407
LightDream <radu.voinea.mihai@gmail.com>
2022-04-30 12:08:48 +0300

    Updated run.sh to auto-restart the web app and its node.

--- .run/run.sh
+++ .run/run.sh
@@ -2,9 +2,19 @@
 
 rm *.jar
 
-# Vars
+# User vars
 HOME=/home/ControlPanel
+SCREEN_NAME_WEB=Web
+SCREEN_NAME_NODE=Node
+WEB_APP_PORT=13000
+
+# Code vars
 CURRENT_DIR=$(pwd)
+
+# Kill the existing processes running on ports for web and node
+# shellcheck disable=SC2046
+kill $(lsof -t -i:"$WEB_APP_PORT")
+screen -dmS $SCREEN_NAME_NODE -X stuff "^C"
 
 # Go to the working directory
 cd $HOME || exit
@@ -23,3 +33,7 @@ rm ControlPanel-Node/target -r
 
 # Go back to the working directory
 cd "$CURRENT_DIR" || exit
+
+# Start the node and the web server
+screen -dmS $SCREEN_NAME_WEB -X stuff "java -jar $HOME/.run/Web.jar\n"
+screen -dmS $SCREEN_NAME_NODE -X stuff "java -jar $HOME/.run/Node.jar\n"
