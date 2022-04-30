0ceb70c8e6bc90af205883ba5e033dcfc0bffc89
LightDream <radu.voinea.mihai@gmail.com>
2022-04-30 11:43:17 +0300

    Added restAPI for /api/server/{serverID}/save

--- ControlPanel-Web/src/main/java/dev/lightdream/controlpanel/controller/rest_end_points/ServerAPIRest.java
+++ ControlPanel-Web/src/main/java/dev/lightdream/controlpanel/controller/rest_end_points/ServerAPIRest.java
@@ -1,5 +1,6 @@
 package dev.lightdream.controlpanel.controller.rest_end_points;
 
+import dev.lightdream.common.database.GlobalPermissionContainer;
 import dev.lightdream.common.database.Node;
 import dev.lightdream.common.database.Server;
 import dev.lightdream.common.database.User;
@@ -10,15 +11,13 @@ import dev.lightdream.common.utils.Utils;
 import dev.lightdream.controlpanel.Main;
 import dev.lightdream.controlpanel.controller.RestEndPoints;
 import dev.lightdream.controlpanel.dto.Log;
+import dev.lightdream.controlpanel.dto.data.impl.ServerData;
 import dev.lightdream.controlpanel.service.ConsoleService;
 import dev.lightdream.logger.Debugger;
 import lombok.SneakyThrows;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -69,7 +68,7 @@ public class ServerAPIRest extends RestEndPoints {
                         }
                     }
 
-                    if(command.getCommand().equals("stop")){
+                    if (command.getCommand().equals("stop")) {
                         ConsoleService.instance.sendConsole(server, new Log("Received stop command", ""));
                     }
 
@@ -131,5 +130,33 @@ public class ServerAPIRest extends RestEndPoints {
                 .parse()
         );
         return Response.OK();
+    }
+
+    @PostMapping("/api/server/{serverID}/save")
+    @ResponseBody
+    public Response nodeSettings(HttpServletRequest request, @CookieValue(value = "login_data", defaultValue = "") String cookieBase64,
+                                 @PathVariable String serverID, @RequestBody ServerData data) {
+
+        dev.lightdream.common.database.Server server = dev.lightdream.common.database.Server.getServer(serverID);
+
+        return executeEndPoint(request, cookieBase64,
+                (user) -> {
+                    if (!data.validate()) {
+                        return Response.BAD_DATA(data.getInvalidFields());
+                    }
+
+                    server.name = data.name;
+                    server.path = data.path;
+                    server.port = data.port;
+                    server.java = data.java;
+                    server.ram = data.ram;
+                    server.serverJar = data.serverJar;
+                    server.args = data.args;
+                    server.startIfOffline = data.startIfOffline;
+                    return Response.OK();
+                },
+                GlobalPermissionContainer.getInstance(), PermissionEnum.GLOBAL_MANAGE_NODES
+        );
+
     }
 }
