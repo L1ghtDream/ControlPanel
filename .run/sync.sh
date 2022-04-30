4a0232168556b64b5200f2868913f35912f00b01
LightDream <radu.voinea.mihai@gmail.com>
2022-04-30 12:09:52 +0300

    Removed functionality from sync.sh to kill the web app (moved to run.sh)

--- .run/sync.sh
+++ .run/sync.sh
@@ -2,10 +2,6 @@
 
 REMOTE=root@htz1.original.gg
 WORKING_DIR="/home/ControlPanel"
-WEB_PORT=13000
-
-# Kill the existing processes running on ports for web and node
-ssh $REMOTE 'kill $(lsof -t -i:'$WEB_PORT')'
 
 # Sync the files from the local machine to the remote machine
 rsync -avh ../. $REMOTE:/$WORKING_DIR --delete
