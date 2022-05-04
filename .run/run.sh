dc17d9fdff212121d8dca9f9352e42f54c925c2a
LightDream <radu.voinea.mihai@gmail.com>
2022-05-04 10:55:47 +0300

    Added more servers to run.sh

--- .run/run.sh
+++ .run/run.sh
@@ -46,4 +46,18 @@ screen -dmS $SCREEN_NAME_WEB -X stuff "java -jar $HOME/.run/Web.jar\n"
 screen -dmS $SCREEN_NAME_NODE -X stuff "java -jar $HOME/.run/Node.jar\n"
 
 echo "Started Web App."
-echo "Started Node."
+
+echo "Starting node on htz-1"
+
+echo "Starting node on htz-5"
+
+ssh root@htz5.original.gg -o StrictHostKeyChecking=no -i $HOME/.run/config/ssh_keys/htz-5 "rm $HOME/.run/Node.jar"
+
+scp -i $HOME/.run/config/ssh_keys/htz-5 $HOME/.run/Node.jar root@htz5.original.gg:/home/ControlPanel/.run/Node.jar
+
+ssh root@htz5.original.gg -o StrictHostKeyChecking=no -i $HOME/.run/config/ssh_keys/htz-5 "screen -dmS $SCREEN_NAME_NODE -X stuff \"^C\""
+sleep .5
+ssh root@htz5.original.gg -o StrictHostKeyChecking=no -i $HOME/.run/config/ssh_keys/htz-5 "screen -dmS $SCREEN_NAME_NODE -X stuff \"^C\""
+
+ssh root@htz5.original.gg -o StrictHostKeyChecking=no -i $HOME/.run/config/ssh_keys/htz-5 "screen -dmS $SCREEN_NAME_NODE -X stuff \"java -jar $HOME/.run/Node.jar\n\""
+
