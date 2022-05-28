7c8f4e3eea1b353ac2330ef9f7ff9d65956c7687
LightDream <radu.voinea.mihai@gmail.com>
2022-05-28 19:17:13 +0300

    Fixed sync.sh

--- .run/sync.sh
+++ .run/sync.sh
@@ -4,7 +4,7 @@ REMOTE=root@htz1.original.gg
 WORKING_DIR="/home/ControlPanel"
 
 # Sync the files from the local machine to the remote machine
-rsync -avh -e "ssh -i config/ssh_keys/htz-1" ../. root@REMOTE:$WORKING_DIR --delete --exclude '.run/config'
+rsync -avh -e "ssh -i config/ssh_keys/htz-1" ../. $REMOTE:$WORKING_DIR --delete --exclude '.run/config'
 
 # Stop all the currently running processes
 sh kill-all.sh
